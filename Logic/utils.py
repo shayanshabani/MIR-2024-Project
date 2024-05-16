24248d646e0bb2e389db4904d36b9f47f94d680d
shayanshabani <shayan.shabani5814@gmail.com>
2024-05-16 19:44:52 +0330

    utils merged

--- Logic/utils.py
+++ Logic/utils.py
@@ -1,20 +1,24 @@
 from typing import Dict, List
-from .core.search import SearchEngine
-from .core.utility.spell_correction import SpellCorrection
-from .core.utility.snippet import Snippet
-from .core.indexer.indexes_enum import Indexes, Index_types
+from Logic.core.search import SearchEngine
+from Logic.core.spell_correction import SpellCorrection
+from Logic.core.snippet import Snippet
+from Logic.core.indexer.indexes_enum import Indexes, Index_types
+from Logic.core.indexer.index_reader import Index_reader
 import json
 
-movies_dataset = None  # TODO: load your movies dataset (from the json file you saved your indexes in), here
-# You can refer to `get_movie_by_id` to see how this is used.
-# search_engine = SearchEngine()
+path = '../Logic/core/indexer/index/documents_index.json'
+with open(path, 'r') as f:
+    json_data = f.read()
+document_index = json.loads(json_data)
+movies_dataset = document_index  # TODO
+search_engine = SearchEngine()
 
 
 def correct_text(text: str, all_documents: List[str]) -> str:
     """
-    Correct the give query text, if it is misspelled using Jacard similarity
+    Correct the give query text, if it is misspelled using Jaccard similarity
 
-    Paramters
+    Parameters
     ---------
     text: str
         The query text
@@ -22,12 +26,13 @@ def correct_text(text: str, all_documents: List[str]) -> str:
         The input documents.
 
     Returns
+    -------
     str
         The corrected form of the given text
     """
     # TODO: You can add any preprocessing steps here, if needed!
-    # spell_correction_obj = SpellCorrection(all_documents)
-    # text = spell_correction_obj.spell_check(text)
+    spell_correction_obj = SpellCorrection(all_documents)
+    text = spell_correction_obj.spell_check(text)
     return text
 
 
@@ -44,34 +49,32 @@ def search(
 
     Parameters
     ---------------------------------------------------------------------------------------------------
-    query:
-        The query text
-
     max_result_count: Return top 'max_result_count' docs which have the highest scores.
                       notice that if max_result_count = -1, then you have to return all docs
 
+    mode: 'detailed' for searching in title and text separately.
+          'overall' for all words, and weighted by where the word appears on.
+
+    where: when mode ='detailed', when we want search query
+            in title or text not both of them at the same time.
+
     method: 'ltn.lnn' or 'ltc.lnc' or 'OkapiBM25'
 
-    weights:
-        The list, containing importance weights in the search result for each of these items:
-            Indexes.STARS: weights[0],
-            Indexes.GENRES: weights[1],
-            Indexes.SUMMARIES: weights[2],
-
-    preferred_genre:
-        A list containing preference rates for each genre. If None, the preference rates are equal.
-        (You can leave it None for now)
+    preferred_genre: A list containing preference rates for each genre. If None, the preference rates are equal.
 
     Returns
     ----------------------------------------------------------------------------------------------------
     list
     Retrieved documents with snippet
     """
-    # weights = ...  # TODO
-    # return search_engine.search(
-    #     query, method, weights, max_results=max_result_count, safe_ranking=True
-    # )
-    return None
+    weights = {
+        Indexes.STARS: weights[0],
+        Indexes.GENRES: weights[1],
+        Indexes.SUMMARIES: weights[2],
+    }
+    return search_engine.search(
+        query, method, weights, max_results=max_result_count, safe_ranking=True
+    )
 
 
 def get_movie_by_id(id: str, movies_dataset: List[Dict[str, str]]) -> Dict[str, str]:
@@ -91,23 +94,22 @@ def get_movie_by_id(id: str, movies_dataset: List[Dict[str, str]]) -> Dict[str, 
     dict
         The movie with the given id
     """
-    # result = movies_dataset.get(
-    #     id,
-    #     {
-    #         "Title": "This is movie's title",
-    #         "Summary": "This is a summary",
-    #         "URL": "https://www.imdb.com/title/tt0111161/",
-    #         "Cast": ["Morgan Freeman", "Tim Robbins"],
-    #         "Genres": ["Drama", "Crime"],
-    #         "Image_URL": "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
-    #     },
-    # )
+    result = movies_dataset.get(
+        id,
+        {
+            "Title": "This is movie's title",
+            "Summary": "This is a summary",
+            "URL": "https://www.imdb.com/title/tt0111161/",
+            "Cast": ["Morgan Freeman", "Tim Robbins"],
+            "Genres": ["Drama", "Crime"],
+            "Image_URL": "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+        },
+    )
 
-    # result["Image_URL"] = (
-    #     "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg"  # a default picture for selected movies
-    # )
-    # result["URL"] = (
-    #     f"https://www.imdb.com/title/{result['id']}"  # The url pattern of IMDb movies
-    # )
-    # return result
-    return None
+    result["Image_URL"] = (
+        "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg"  # a default picture for selected movies
+    )
+    result["URL"] = (
+        f"https://www.imdb.com/title/{result['id']}"  # The url pattern of IMDb movies
+    )
+    return result
