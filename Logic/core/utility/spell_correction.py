582c932c8f9a1bf9731bc1be9fa9b8eb046a121d
shayanshabani <shayan.shabani5814@gmail.com>
2024-05-16 19:33:36 +0330

    spell correction merged

--- Logic/core/utility/spell_correction.py
+++ Logic/core/utility/spell_correction.py
@@ -27,11 +27,16 @@ class SpellCorrection:
             A set of shingles.
         """
         shingles = set()
-        
+
         # TODO: Create shingle here
 
+        for i in range(len(word) - 1):
+            shingle = ''
+            for j in range(k):
+                shingle += word[i + j]
+            shingles.add(shingle)
         return shingles
-    
+
     def jaccard_score(self, first_set, second_set):
         """
         Calculate jaccard score.
@@ -50,8 +55,13 @@ class SpellCorrection:
         """
 
         # TODO: Calculate jaccard score here.
-
-        return
+        union = len(first_set.union(second_set))
+        intersection = len(first_set.intersection(second_set))
+        if union != 0:
+            jaccard_score = intersection / union
+        else:
+            jaccard_score = 0
+        return jaccard_score
 
     def shingling_and_counting(self, all_documents):
         """
@@ -73,9 +83,19 @@ class SpellCorrection:
         word_counter = dict()
 
         # TODO: Create shingled words dictionary and word counter dictionary here.
-                
+
+        for document in all_documents:
+            words = document.split()
+            for word in words:
+                shingle_set = self.shingle_word(word)
+                if word not in all_shingled_words:
+                    all_shingled_words[word] = shingle_set
+                if word not in word_counter:
+                    word_counter[word] = 0
+                word_counter[word] += 1
+
         return all_shingled_words, word_counter
-    
+
     def find_nearest_words(self, word):
         """
         Find correct form of a misspelled word.
@@ -93,9 +113,46 @@ class SpellCorrection:
         top5_candidates = list()
 
         # TODO: Find 5 nearest candidates here.
+        word_shingles = self.shingle_word(word)
+
+        similarities = {}
+        for candidate_word, candidate_shingles in self.all_shingled_words.items():
+            similarities[candidate_word] = self.jaccard_score(word_shingles, candidate_shingles)
+
+        nearest_words = sorted(similarities.items(), key=lambda x: x[1], reverse=True)[:5]
+
+        for word, _ in nearest_words:
+            top5_candidates.append(word)
 
         return top5_candidates
-    
+
+    def count_embedding(self, top5_candidates, word):
+        """
+        Calculate total scores.
+
+        Parameters
+        ----------
+        top5_candidates : list of str
+            List of 5 nearest words.
+        word : str
+            The misspelled word.
+
+        Returns
+        -------
+        str
+            The best candidate word.
+        """
+        word_shingles = self.shingle_word(word)
+        normalized_tf = {}
+        max_tf = max(self.word_counter[candidate] for candidate in top5_candidates)
+        for candidate in top5_candidates:
+            candidate_shingles = self.all_shingled_words[candidate]
+            candidate_tf = self.word_counter[candidate]
+            candidate_jaccard = self.jaccard_score(word_shingles, candidate_shingles)
+            normalized_tf[candidate] = (candidate_jaccard * candidate_tf) / max_tf
+        best_candidate = max(normalized_tf, key=normalized_tf.get)
+        return best_candidate
+
     def spell_check(self, query):
         """
         Find correct form of a misspelled query.
@@ -111,7 +168,15 @@ class SpellCorrection:
             Correct form of the query.
         """
         final_result = ""
-        
-        # TODO: Do spell correction here.
 
+        # TODO: Do spell correction here.
+        words = query.split()
+        for word in words:
+            if word in self.word_counter:
+                final_result += word + ' '
+            else:
+                top5_candidates = self.find_nearest_words(word)
+                best_candidate = self.count_embedding(top5_candidates, word)
+                final_result += best_candidate + ' '
+        final_result = final_result.strip()
         return final_result
